aa62015f91e9178ba2b69234ea22cac2be3b6298
Samuel Rosenbluth <samuelrosenbluth@Samuels-MacBook-Air.local>
2016-09-09 10:11:05 -0500

    the project with HTTPS to fixed api issue

--- app.js
+++ app.js
@@ -4,19 +4,13 @@ $(document).ready(function() {
             var fighterData;
             var nextFight;
             $.ajax({
-                url: "http://cors-anywhere.herokuapp.com/http://ufc-data-api.ufc.com/api/v3/iphone/events",
+                url: "https://cors-anywhere.herokuapp.com/http://ufc-data-api.ufc.com/api/v3/iphone/events",
                 dataType: "json",
                 type: "GET"
 
-            }).done(function(data) {
-                    // function ufcResults(data) {
-                       nextFight = data;
-                    // };
-                    console.log(nextFight);
-                  });
-
+            })
                 $.ajax({
-                    url: " http://galvanize-cors-proxy.herokuapp.com/http://ufc-data-api.ufc.com/api/v3/us/fighters",
+                    url: " https://galvanize-cors-proxy.herokuapp.com/http://ufc-data-api.ufc.com/api/v3/us/fighters",
                     dataType: "json",
                     type: "GET"
 
@@ -39,32 +33,32 @@ $(document).ready(function() {
 
                 $("#searchUFC").click(function() {
                     var userInput = $("#fighterName").val()
-                    //console.log("test this button");
-                    //console.log (typeof userInput);
+                        //console.log("test this button");
+                        //console.log (typeof userInput);
                     var myFighterTable = $("#myFighterTable")
                     var userFighter = document.createElement("td");
                     var addFighter = $('.myFighters')
-                    //var addFighter2 = document.getElementsByClassName('myFighters');
-                    //document.body.table.tbody.tr.appendChild(userFighter);
-                    //console.log(fighterData);
+                        //var addFighter2 = document.getElementsByClassName('myFighters');
+                        //document.body.table.tbody.tr.appendChild(userFighter);
+                        //console.log(fighterData);
                     for (var i = 0; i < fighterData.length; i++) {
                         if (userInput.toLowerCase() === fighterData[i].first_name.toLowerCase() ||
                             userInput.toLowerCase() === fighterData[i].last_name.toLowerCase()) {
                             $('#myFighterTable tr:last').after(
                                 '<tr> <td>' +
-                                 fighterData[i].first_name +
-                                 " " +
-                                 fighterData[i].last_name +
-                                 '</td><td>'+ fighterData[i].nickname +
-                                 '</td><td> <img src="' + fighterData[i].thumbnail + '"/>' +
+                                fighterData[i].first_name +
+                                " " +
+                                fighterData[i].last_name +
+                                '</td><td>' + fighterData[i].nickname +
+                                '</td><td> <img src="' + fighterData[i].thumbnail + '"/>' +
 
 
-                                 '</td><td>' + fighterData[i].weight_class +
-                                 '</td><td>' + fighterData[i].wins +
-                                 '</td><td>' + fighterData[i].losses +
-                                 '</td><td>' + fighterData[i].draws +
+                                '</td><td>' + fighterData[i].weight_class +
+                                '</td><td>' + fighterData[i].wins +
+                                '</td><td>' + fighterData[i].losses +
+                                '</td><td>' + fighterData[i].draws +
 
-                                 '</td> </tr>')
+                                '</td> </tr>')
 
 
 
